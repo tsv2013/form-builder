8713a241d7e224faf3342856ad4c0108505e8317
tsv2013 <tsv2013@users.noreply.github.com>
2020-12-01 22:29:36 +0300

    Added test composite element to toolbox

--- index.js
+++ index.js
@@ -43,6 +43,26 @@ var toolboxItems = [
                 }
             ]
         }
+    },
+    {
+        title: "composite",
+        hint: "Drag to add composite element",
+        json: {
+            partclass: "layoutItem",
+            cssClasses: "item test-item",
+            parts: [
+                {
+                    partclass: "label",
+                    cssClasses: "test-label",
+                    data: "Label text"
+                },
+                {
+                    partclass: "input",
+                    cssClasses: "test-input",
+                    data: "valName"
+                }
+            ]
+        }
     }
 ];
 FormBuilder.render(layoutJson, toolboxItems, document.getElementById("form-builder-container"));
